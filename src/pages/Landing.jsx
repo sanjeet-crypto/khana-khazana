6e3f5fc6c532176ed2463150103b9933c5bb6185
Sayantan Chatterjee <sayantan1298@gmail.com>
2021-10-31 01:23:26 +0530

    Fixed NavBar , Made Testimonial Page , Chefs Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -23,6 +23,7 @@ const Landing = () => {
                 </div>
             </div>
         </div>
+        
         </>
     )
 }
---
+++ src/pages/chefData.js
@@ -0,0 +1,33 @@
+const Chefdata= [
+    {
+        imgsrc: "https://image.freepik.com/free-vector/happy-chef-character-with-two-plates_23-2147629980.jpg",
+        name: "Sayantan Chatterjee" , 
+        desc: "Masters in Indian and Chinese"
+    },
+    {
+        imgsrc: "https://image.freepik.com/free-vector/happy-chef-character-with-two-plates_23-2147629980.jpg",
+        name: "Shreya Khetan" , 
+        desc: "Masters in Nepali and Chinese"
+    },
+    {
+        imgsrc: "https://image.freepik.com/free-vector/happy-chef-character-with-two-plates_23-2147629980.jpg",
+        name: "Soumya Patel" , 
+        desc: "Masters in Anglo and Asian"
+    },
+    {
+        imgsrc: "https://image.freepik.com/free-vector/happy-chef-character-with-two-plates_23-2147629980.jpg",
+        name: "Debrup Panda" , 
+        desc: "Masters in American and Pan-Asian"
+    },
+    {
+        imgsrc: "https://image.freepik.com/free-vector/happy-chef-character-with-two-plates_23-2147629980.jpg",
+        name: "Sanjeet Patro" , 
+        desc: "Masters in Greek and Mexican"
+    },
+    {
+        imgsrc: "https://image.freepik.com/free-vector/happy-chef-character-with-two-plates_23-2147629980.jpg",
+        name: "Shantanu Ramesh" , 
+        desc: "Masters in Indian and American"
+    },
+]
+export default Chefdata;
